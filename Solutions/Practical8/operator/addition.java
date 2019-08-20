7b49c81c4636f2116e5bb6f293327a500f761252
Naman Dangi <namandangi@users.noreply.github.com>
2019-08-20 21:20:16 +0530

    Add package operator with addition.java and addition.class

---
+++ Solutions/Practical8/operator/addition.java
@@ -0,0 +1,20 @@
+package operator;
+import java.util.*;
+public class addition
+{
+int i,j;
+public int add(int a,int b)
+{
+	return a+b;
+}
+ public static void main(String []args)
+ {
+//	int s=0;
+// 	Scanner sc = new Scanner(System.in);
+//	System.out.println("Enter 2 nos to add");
+// 	a = sc.nextInt();
+// 	b = sc.nextInt();
+// 	addition a = new addition(); //	s = a.add(a,b);
+// 	System.out.println("The sum is : "+s);
+ 	}
+} 	
