049b9b2c04f9842955ae06398a03e4d4ab1ff9aa
Naman Dangi <namandangi@users.noreply.github.com>
2019-08-20 21:16:50 +0530

    Add volume.class and volume.java

---
+++ Solutions/Practical5/volume.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class volume
+{
+	double v;
+	final double pi =3.142;
+	public static void main(String []args)
+	{
+		int c,s,l,b,h;
+		Scanner sc = new Scanner(System.in);
+		volume ob = new volume();
+		do{
+			System.out.println("Enter your choice");
+			System.out.println("0. EXIT");
+			System.out.println("1. CUBE");
+			System.out.println("2. CUBOID");
+			c =sc.nextInt();
+		switch(c)
+		{
+			case 0:
+			break;
+		
+			case 1:
+			{
+				System.out.println("Enter the side");
+				s=sc.nextInt();
+				ob.volume(s);
+				break;
+			}
+			case 2:
+			{
+				System.out.println("Enter the length");
+				l=sc.nextInt();
+				System.out.println("Enter the breadth");
+				b=sc.nextInt();
+				System.out.println("Enter the height");
+				h=sc.nextInt();
+				ob.volume(l,b,h);
+				break;			
+		 	}
+			default:
+				System.out.println("Please check Yo input");
+		}
+		
+		}while(c!=0);
+	}
+		void volume(int s)
+			{
+			v=s*s*s;
+			System.out.println("volume is : "+v);
+		}
+		void volume(int l,int b,int h)
+		{
+			v=l*b*h;
+			System.out.println("volume is : "+v);
+		}	
+	}			
